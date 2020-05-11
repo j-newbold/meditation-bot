69475e4b7d89ead857990689ee0f31dd59ad14f5
Jesse <jnewbold@conncoll.edu>
2020-05-10 22:59:51 -0400

    heroku fix test 5

--- cmds/add.js
+++ cmds/add.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 
     const dbClient = new Client({
         connectionString: process.env.DATABASE_URL,
-        ssl: false, // This is not ideal
+        // ssl: false, // I am not sure if this is ideal
     });
     var mins = Number(args[0]);
     if (Number.isInteger(mins)) {
